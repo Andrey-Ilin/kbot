798e55eca750dd93a4be322a9ed94e480860063b
Andrii Ilin <andrey.ilyin83@gmail.com>
2024-09-03 01:53:24 +0000

    add initial logger logic to the app

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -10,12 +10,71 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
+
+	"github.com/hirosassa/zerodriver"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+
 	telebot "gopkg.in/telebot.v3"
 )
 
 var (
 	TeleToken = os.Getenv("TELE_TOKEN")
+	MetricsHost = os.Getenv("METRICS_HOST")
 )
+
+// Initialize OpenTelemetry
+func initMetrics(ctx context.Context) {
+
+	// Create a new OTLP Metric gRPC exporter with the specified endpoint and options
+	exporter, _ := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithEndpoint(MetricsHost),
+		otlpmetricgrpc.WithInsecure(),
+	)
+
+	// Define the resource with attributes that are common to all metrics.
+	// labels/tags/resources that are common to all metrics.
+	resource := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(fmt.Sprintf("kbot_%s", appVersion)),
+	)
+
+	// Create a new MeterProvider with the specified resource and reader
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(
+			// collects and exports metric data every 10 seconds.
+			sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(10*time.Second)),
+		),
+	)
+
+	// Set the global MeterProvider to the newly created MeterProvider
+	otel.SetMeterProvider(mp)
+
+}
+
+func pmetrics(ctx context.Context, payload string) {
+	// Get the global MeterProvider and create a new Meter with the name "kbot_light_signal_counter"
+	meter := otel.GetMeterProvider().Meter("kbot_weather_by_city")
+
+	message := "";
+
+	if strings.HasPrefix(strings.ToLower(payload), "weather ") {
+		message = strings.TrimPrefix(strings.ToLower(payload), "weather ")
+	} else {
+		return
+	}
+
+	// Get or create an Int64Counter instrument with the name "kbot_weather_by_<message>"
+	counter, _ := meter.Int64Counter(fmt.Sprintf("kbot_weather_by_%s", message))
+
+	// Add a value of 1 to the Int64Counter
+	counter.Add(ctx, 1)
+}
 
 func getWeather(city string) (string, error) {
 	url := fmt.Sprintf("https://wttr.in/%s?format=3", city)
@@ -46,7 +105,8 @@ var kbotCmd = &cobra.Command{
 	Short:   "Start the Telegram bot",
 	Long:    `Start the Telegram bot and begin listening for messages.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("kbot %s started\n", appVersion)
+
+		logger := zerodriver.NewProductionLogger();
 
 		kbot, err := telebot.NewBot(telebot.Settings{
 			URL:    "",
@@ -55,13 +115,18 @@ var kbotCmd = &cobra.Command{
 		})
 
 		if err != nil {
-			log.Fatalf("Please check TELE_TOKEN env variable, %s", err)
+			logger.Fatalf("Please check TELE_TOKEN env variable, %s", err)
 			return
+		} else {
+			logger.Info().Str("Version", appVersion).Msg("kbot started")
 		}
 
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Printf("Received message: %s", m.Text())
+			logger.Info().Str("Payload", m.Text()).Msg(m.Message().Payload)
 			msg := m.Text()
+
+			pmetrics(context.Background(), msg)
+
 			var err error
 
 			if strings.HasPrefix(strings.ToLower(msg), "weather ") {
@@ -96,6 +161,7 @@ var kbotCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(kbotCmd)
+	initMetrics(ctx)
 
 	// Here you will define your flags and configuration settings.
 
